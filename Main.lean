0471841e6244fce390a77cc7705d23be911a281b
Joachim Breitner <mail@joachim-breitner.de>
2026-01-12 20:52:05 +0000

    Hack: Remove checkQuotInit

--- Main.lean
+++ Main.lean
@@ -223,16 +223,6 @@ def checkPostponedRecursors : M Unit := do
       unless info == info' do throw <| IO.userError s!"Invalid recursor {ctor}"
     | _, _ => throw <| IO.userError s!"No such recursor {ctor}"
 
-/--
-Check that at the end of (any) file, the quotient module is initialized by the end.
-(It will already be initialized at the beginning, unless this is the very first file,
-`Init.Core`, which is responsible for initializing it.)
-This is needed because it is an assumption in `finalizeImport`.
--/
-def checkQuotInit : M Unit := do
-  unless (← get).env.quotInit do
-    throw <| IO.userError s!"initial import (Init.Core) didn't initialize quotient module"
-
 /-- "Replay" some constants into an `Environment`, sending them to the kernel for checking. -/
 def replay (ctx : Context) (env : Environment) (decl : Option Name := none) :
     IO (Nat × Environment) := do
@@ -251,7 +241,6 @@ def replay (ctx : Context) (env : Environment) (decl : Option Name := none) :
           replayConstant n
       checkPostponedConstructors
       checkPostponedRecursors
-      checkQuotInit
   return (s.numAdded, s.env)
 
 open private ImportedModule.mk from Lean.Environment in
